0505f1fa66ecff8808572de366baf3fb92c4a2ec
Shital Chaudhary <shitalchau10@gmail.com>
2021-09-03 16:46:14 -0500

    create a delete route

--- server.js
+++ server.js
@@ -21,36 +21,79 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
 );
+// show all candidates
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
 
-// db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(row);   
-//     }  
-// });
+    db.query(sql, (err, rows) => {
+        if(err){
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+    });
 
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+});
+
+
+// show a single candidate
+
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        } 
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+
+});
+
+// delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+    if (err) {
+        res.statusMessage(400).json({ error: res.message });
+        return;
+    } else if (!result.affectedRows){
+        res.json({
+            message: 'Candidate not found'
+        });
+    } else{
+        res.json({
+            message: 'deleted', 
+            changes: result.affectedRows,
+            id: req.params.id
+         });
+    } 
+    });
+});
+
+
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
 //     if (err) {
 //         console.log(err);
 //     }         
-//     console.log(result);   
+//     console.log(result);  
 // });
-
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
-    }         
-    console.log(result);  
-});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
