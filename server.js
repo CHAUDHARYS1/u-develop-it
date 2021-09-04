922d656e20d5daa1e319a30365c34ee216561cb8
Shital Chaudhary <shitalchau10@gmail.com>
2021-09-04 11:57:20 -0500

    create and populate table for parties

--- server.js
+++ server.js
@@ -104,18 +104,20 @@ app.post('/api/candidate', ({
 
     const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
     VALUES (?,?,?)`;
-  const params = [body.first_name, body.last_name, body.industry_connected];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
     });
-  });
 });
 
 
