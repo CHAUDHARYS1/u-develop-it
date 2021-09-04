b6111a6d37cb024b0fc12b95d3708b0408bad708
Shital Chaudhary <shitalchau10@gmail.com>
2021-09-04 15:06:13 -0500

    update candidate routes to join with party data

--- server.js
+++ server.js
@@ -47,12 +47,7 @@ app.get('/api/candidates', (req, res) => {
 // show a single candidate
 
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
-    AS party_name 
-    FROM candidates 
-    LEFT JOIN parties 
-    ON candidates.party_id = parties.id 
-    WHERE candidates.id = ?`;
+    const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
