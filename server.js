17e6328d08cc425a2de4e2fdb7ec9870d07db7d5
Shital Chaudhary <shitalchau10@gmail.com>
2021-09-04 15:25:26 -0500

    create api routes for parties

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const db = mysql.createConnection({
     },
     console.log('Connected to the election database.')
 );
-// show all candidates
 
+// show all candidates
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id`;
 
@@ -43,9 +43,7 @@ app.get('/api/candidates', (req, res) => {
 
 });
 
-
 // show a single candidate
-
 app.get('/api/candidate/:id', (req, res) => {
     const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id WHERE candidates.id = ?`;
     const params = [req.params.id];
@@ -120,6 +118,62 @@ app.post('/api/candidate', ({
     });
 });
 
+// show all parties
+app.get('/api/parties/', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({
+                error: err.message
+            });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// show individual parties
+app.get('/api/parties/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql,params, (err, row) => {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// delete parties
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        // checks if anything was deleted
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Party not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
